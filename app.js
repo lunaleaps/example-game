0f2b5483eb7545f2b89efa4d18175f44d270387c
Luna Wei <luna.wei@kik.com>
2014-12-14 15:35:30 -0500

    try changing port

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ var router = Router({static_route: __dirname + '/static'});
 router.get('/hello', function(req, res) {
 	res.end('hello world');});
 var app = http.createServer(router);
-app.listen(5555);
+app.listen(8080);
 
