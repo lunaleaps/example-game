d8356f1fe853163beda248d1378747fcf1fa74b8
Luna Wei <luna.wei@kik.com>
2014-12-14 15:19:45 -0500

    re-init

---
+++ app.js
@@ -0,0 +1,10 @@
+var http    = require('http'),
+	Router  = require('node-simple-router');
+
+var router = Router({static_route: __dirname + '/static'});
+
+router.get('/hello', function(req, res) {
+	res.end('hello world');});
+var app = http.createServer(router);
+app.listen(5555);
+
